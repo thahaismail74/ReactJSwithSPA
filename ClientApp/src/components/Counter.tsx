90d725cbb2148995d52e6451b5cec57b648e694b
thahaismail74 <44117864+thahaismail74@users.noreply.github.com>
2020-12-01 20:56:50 +0530

    changed the order of import libraries

--- ClientApp/src/components/Counter.tsx
+++ ClientApp/src/components/Counter.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
-import { ApplicationState } from '../store';
 import * as CounterStore from '../store/Counter';
+import { ApplicationState } from '../store';
 
 type CounterProps =
     CounterStore.CounterState &
